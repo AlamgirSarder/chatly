47f308238b7cd8461bf576ecbb29217869d04507
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-06-17 22:31:00 +0600

    chating application

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+
+        <section>
+            <h2>welcome chating application</h2>
+            <h1>Bangladesh</h1>
+        </section>
+
+  )
+}
+
+export default Home
+
---
+++ src/components/layout/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = ({children}) => {
+  return (
+    <div className='w-[800px] bg-red-500 m-auto'>
+        {children}
+    </div>
+  )
+}
+
+export default Container
+
+
