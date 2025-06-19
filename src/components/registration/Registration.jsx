03b5db6c0007d592614a7b03c6eb3204e57ea2ab
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-06-20 01:20:18 +0600

    registration page design complete

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -7,7 +7,7 @@ const Registration = () => {
   return (
     <Container>
       <div className="flex justify-between">
-        <div className="w-[756px] pt-[225px] pl-[190px]">
+        <div className="w-[756px] pt-[120px] pl-[120px]">
           <h2 className="font-primary text-primary text-[35px] font-bold mb-[13px]">
             Get started with easily register
           </h2>
@@ -15,91 +15,122 @@ const Registration = () => {
             Free register and you can enjoy it
           </p>
 
-          <div className="w-[368px] flex flex-col gap-[56px]">
-            {" "}
-            {/* adds spacing between inputs */}
-            <TextField
-              label="Email Address"
-              variant="outlined"
-              type="text"
-              fullWidth
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "8px",
-                  "& fieldset": {
-                    borderColor: "#ccc",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#6366f1", // Tailwind indigo-500
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#4ade80", // Tailwind green-400
-                  },
-                },
-                "& .MuiInputLabel-root": {
-                  fontSize: "16px",
-                  color: "#6b7280", // gray-500
-                },
-                "& .Mui-focused .MuiInputLabel-root": {
-                  color: "#4ade80",
-                },
-              }}
-            />
-            <TextField
-              label="Full Name"
-              variant="outlined"
-              type="text"
-              fullWidth
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "8px",
-                  "& fieldset": {
-                    borderColor: "#ccc",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#6366f1",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#4ade80",
-                  },
-                },
-                "& .MuiInputLabel-root": {
-                  fontSize: "16px",
-                  color: "#6b7280",
-                },
-                "& .Mui-focused .MuiInputLabel-root": {
-                  color: "#4ade80",
-                },
-              }}
-            />
-            <TextField
-              label="Password"
-              variant="outlined"
-              type="password"
-              fullWidth
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "8px",
-                  "& fieldset": {
-                    borderColor: "#ccc",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#6366f1",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#4ade80",
-                  },
-                },
-                "& .MuiInputLabel-root": {
-                  fontSize: "16px",
-                  color: "#6b7280",
-                },
-                "& .Mui-focused .MuiInputLabel-root": {
-                  color: "#4ade80",
-                },
-              }}
-            />
-          </div>
+          <form>
+            <div className="w-[368px]">
+              <div className="mb-[56px]">
+                <TextField
+                  label="Email Address"
+                  variant="outlined"
+                  type="text"
+                  fullWidth
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "8px",
+                      "& fieldset": {
+                        borderColor: "#11175D",
+                        opacity:"0.3",
+                    
+                      },
+                      "&:hover fieldset": {
+                         borderColor: "#11175D",
+                        opacity:"0.3",
+                      },
+                      "&.Mui-focused fieldset": {
+                           borderColor: "#11175D",
+                        opacity:"0.3",
+                      },
+                    },
+                    "& .MuiInputLabel-root": {
+                      fontSize: "14px",
+                      color: "#11175D",
+                      opacity:"0.7",
+                    },
+                    "& .Mui-focused .MuiInputLabel-root": {
+                      color: "#4ade80",
+                    },
+                  }}
+                />
+              </div>
+
+              <div className="mb-[56px]"><TextField
+                label="Full Name"
+                variant="outlined"
+                type="text"
+                fullWidth
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "8px",
+                      "& fieldset": {
+                        borderColor: "#11175D",
+                        opacity:"0.3",
+                    
+                      },
+                      "&:hover fieldset": {
+                         borderColor: "#11175D",
+                        opacity:"0.3",
+                      },
+                      "&.Mui-focused fieldset": {
+                           borderColor: "#11175D",
+                        opacity:"0.3",
+                      },
+                    },
+                    "& .MuiInputLabel-root": {
+                      fontSize: "14px",
+                      color: "#11175D",
+                      opacity:"0.7",
+                    },
+                    "& .Mui-focused .MuiInputLabel-root": {
+                      color: "#4ade80",
+                    },
+                  }}
+          
+              /></div>
+              
+             <div> <TextField
+                label="Password"
+                variant="outlined"
+                type="password"
+                fullWidth
+               sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "8px",
+                      "& fieldset": {
+                        borderColor: "#11175D",
+                        opacity:"0.3",
+                    
+                      },
+                      "&:hover fieldset": {
+                         borderColor: "#11175D",
+                        opacity:"0.3",
+                      },
+                      "&.Mui-focused fieldset": {
+                           borderColor: "#11175D",
+                        opacity:"0.3",
+                      },
+                    },
+                    "& .MuiInputLabel-root": {
+                      fontSize: "14px",
+                      color: "#11175D",
+                      opacity:"0.7",
+                    },
+                    "& .Mui-focused .MuiInputLabel-root": {
+                      color: "#4ade80",
+                    },
+                  }}
+              /></div>
+
+              <button className="relative py-[19px] w-full bg-[#1E1E1E] font-primary text-[21px] font-semibold text-[#FFFFFF] rounded-[86px] after:content-[''] after:absolute after:w-[71px] after:h-[28px] after:rounded-[86px] after:bg-[#5B36F5] after:top-1/2 after:left-1/2 after:blur-[43px] after:-translate-1/2 after:-z-1 z-1 cursor-pointer shadow-[0_4px_4px_rgba(0,0,0,0.25)] mt-[52px]">
+                Sign up
+              </button>
+
+              <p className="mt-[35px] text-center text-[#03014C] font-secondary text-[13px]">
+                Already have an account ?{" "}
+                <span className="text-[#EA6C00] font-secondary font-bold text-[13px]">
+                  Sign In
+                </span>
+              </p>
+            </div>
+          </form>
         </div>
 
         <div className="w-[684px]">
