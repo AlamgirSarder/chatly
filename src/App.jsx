27a2063e2e12177ad1515d52ff79cb943de24688
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-06-25 01:24:37 +0600

    router change

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+// import { createBrowserRouter, RouterProvider } from "react-router";
+// import Registration from "./pages/Registration";
+// import Login from "./pages/Login";
+
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/registration",
+//       element: <Registration />,
+//     },
+//     {
+//       path: "/login",
+//       element: <Login />,
+//     },
+//   ],
+//   {
+//     basename: "/chating-application",
+//   }
+// );
+
+// function App() {
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import { HashRouter as Router, Routes, Route } from "react-router";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/registration",
-      element: <Registration />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ],
-  {
-    basename: "/chating-application",
-  }
-);
-
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <Router basename="/">
+      <Routes>
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
