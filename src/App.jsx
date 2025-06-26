ebc60b8f1a5614e1e0918a66833e83f19eb1064c
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-06-27 00:35:57 +0600

    gh-pages delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,31 @@
-// import { createBrowserRouter, RouterProvider } from "react-router";
-// import Registration from "./pages/Registration";
-// import Login from "./pages/Login";
-
-// const router = createBrowserRouter(
-//   [
-//     {
-//       path: "/registration",
-//       element: <Registration />,
-//     },
-//     {
-//       path: "/login",
-//       element: <Login />,
-//     },
-//   ],
-//   {
-//     basename: "/chating-application",
-//   }
-// );
-
-// function App() {
-//   return (
-//     <>
-//       <RouterProvider router={router} />
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-import { HashRouter as Router, Routes, Route } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/registration",
+      element: <Registration />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ],
+
+);
+
 function App() {
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
-
 export default App;
+
+
+
+
