cebcdff4fd497b63d1336ec503ed2ab6e2c9be5d
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-07-01 01:41:00 +0600

    home image add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import { Container } from '@mui/material'
 import React from 'react'
+import home_image from '../assets/home_image.png'
 
 const Home = () => {
+  
+
   return (
     <div className='p-[30px]'>
         <Container className='p-[30px]'>
 
-          <div className='w-[186px] h-[954px] bg-bggcolor rounded-[20px]'>
+          <div className='flex justify-center w-[186px] h-[954px] bg-bggcolor rounded-[20px]'>
+            <div className='size-[100px] bg-gray-500 rounded-full mt-[38px]'>
+                <img src={home_image} alt="#home_image" />
+            </div>
 
           </div>
         
