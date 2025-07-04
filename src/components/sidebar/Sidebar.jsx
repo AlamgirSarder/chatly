9366ebff57a985ecb44c364d7f65e12b8cbc85b0
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-07-05 00:09:05 +0600

    change

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,24 @@
 import home_image from "../../assets/home_image.png";
 import Container from "../layout/Container";
+import { SlHome } from "react-icons/sl";
 const Sidebar = () => {
   return (
     <div>
       <Container>
         <div className="p-[30px]">
-          <div className="flex justify-center w-[186px] h-[954px] bg-bggcolor rounded-[20px]">
-            <div className="size-[100px] bg-gray-500 rounded-full mt-[38px]">
+          <div className="w-[186px] h-[954px] bg-bggcolor rounded-[20px] pt-[38px]">
+
+            <div className="size-[100px] bg-gray-500 rounded-full mx-auto mb-[78px]">
               <img src={home_image} alt="#home_image" />
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
+
+            <div className="py-[20px] ">
+                <SlHome size={46}  />
+            </div>
+
+
+
+
           </div>
         </div>
       </Container>
